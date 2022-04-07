37a9263e1c2a3b3d6d5229fe3beb2530d61b2a81
Yared Zemaw <yarazemaw111@gmail.com>
2022-04-07 20:00:22 +0900

    commit 0x00

---
+++ 5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * My hello, print task
+ * Return: return 1 after program excution
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcme is a piece of art.\n");
+return (0);
+}
